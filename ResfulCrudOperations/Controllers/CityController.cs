99ba25a8fe0f0e92b35b7533d830e8f778c341b9
pochamreddy kavya <kavya.pocham@gmail.com>
2022-10-23 16:50:58 +0100

    Initial Checkin

---
+++ ResfulCrudOperations/Controllers/CityController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResfulCrudOperations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResfulCrudOperations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+
+        CityContext context = null;
+
+        public CityController(CityContext _obj)
+        {
+            context = _obj;
+        }
+
+        [HttpPost]
+        public IActionResult AddCity(City city)
+        {
+            context.City.Add(city);
+            context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{cityId}")]
+        public IActionResult DeleteCity(int cityId)
+        {
+            var obj=context.City.Where(x => x.CityId == cityId).FirstOrDefault();
+            context.Remove(obj);
+            context.SaveChanges();
+
+            return NoContent();
+        }
+
+       
+        [HttpPut]
+        public IActionResult UpdateCity(City city)
+        {
+            var obj = context.City.Where(x=>x.CityId==city.CityId).FirstOrDefault();
+            obj.CityName = city.CityName;
+            obj.DateEstablished = city.DateEstablished;
+            obj.State = city.State;
+            obj.TouristRating = city.TouristRating;
+            context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+
+}
---
+++ ResfulCrudOperations/Models/CityContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResfulCrudOperations.Models
+{
+    public class CityContext: DbContext
+    {
+        public CityContext(DbContextOptions<CityContext> options) : base(options) { }
+        public DbSet<City>City {get; set; }
+    }
+    public class City
+    { 
+        [Key]
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string State { get; set; }
+        public int TouristRating { get; set; }
+        public DateTime DateEstablished { get; set; }
+        public int EstimatedPopulation{ get; set;}
+    }
+}
